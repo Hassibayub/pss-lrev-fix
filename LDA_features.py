3aabb402b0ffcf8e9489ee40ba1274db59218294
Muhammad Haseeb <53309104+Hassibayub@users.noreply.github.com>
2023-04-19 14:32:09 +0500

    git add LDA_features.py

---
+++ LDA_features.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import model
+
+
+# In[3]:
+
+
+data_train = model.read_csv_data("data/archive20k/text/dataset.train")
+data_test = model.read_csv_data("data/archive20k/text/dataset.test")
+
+
+# In[ ]:
+
+
+
+
